3a736a8cdf52a04a78a2f9f3f1ae0ab32931048c
Burçin Özdemir <114526920+8urcin@users.noreply.github.com>
2023-01-19 21:11:57 +0300

    Update counter.sol

--- counter.sol
+++ counter.sol
@@ -1,17 +1,24 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
+    // Declare a public variable "count" of type uint (unsigned integer)
     uint public count;
 
+    // Function to increment the value of "count" by 1
     function increment() public {
+        // increment the value of count by 1
         count++;
     }
 
+    // Function to decrement the value of "count" by 1
     function decrement() public {
+        // decrement the value of count by 1
         count--;
     }
 
+    // Function to retrieve the current value of "count"
     function getCount() public view returns (uint) {
+        // return the current value of count
         return count;
     }
 }
